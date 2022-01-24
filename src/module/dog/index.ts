cfb6ee76b9859a235028ce0397b8516f3b1bb466
Eric Silva <ericsilvaccp@gmail.com>
2022-01-23 22:34:14 -0300

    feat(presentation): ensure get /dogs call ShowDogsProvider

--- src/module/dog/index.ts
+++ src/module/dog/index.ts
@@ -1,7 +1,14 @@
+import { ShoowDogs } from '@data/dogs/show'
 import { Module } from '@nestjs/common'
 import { ShowDogsController } from '@presentation/controllers/show-dogs.controller'
 
 @Module({
-  controllers: [ShowDogsController]
+  controllers: [ShowDogsController],
+  providers: [
+    {
+      provide: 'ShowDogsProvider',
+      useClass: ShoowDogs
+    }
+  ]
 })
 export class DogModule {}
---
+++ src/presentation/controllers/show-dogs.controller/show-dogs.controller.test.ts
@@ -0,0 +1,35 @@
+import { DogModel } from '@domain/model/dog'
+import { ShowDogsProvider } from '@domain/useCases/dog/show'
+import { ShowDogsController } from '.'
+
+describe('ShowDogsController', () => {
+  const makeShowDogsProviderStub = (): ShowDogsProvider => {
+    class Stub implements ShowDogsProvider {
+      showDogs(): Promise<DogModel[]> {
+        const listDogs: DogModel[] = [{ name: 'any name', breed: 'any breed' }]
+        return Promise.resolve(listDogs)
+      }
+    }
+    return new Stub()
+  }
+
+  const makeSut = () => {
+    const showDogsProviderStub = makeShowDogsProviderStub()
+    const sut = new ShowDogsController(showDogsProviderStub)
+    return {
+      sut,
+      showDogsProviderStub
+    }
+  }
+
+  describe('GET - dogs', () => {
+    it('should get /dogs call ShowDogsProvider', async () => {
+      const { sut, showDogsProviderStub } = makeSut()
+      const spyShowDogs = jest.spyOn(showDogsProviderStub, 'showDogs')
+
+      await sut.handler()
+
+      expect(spyShowDogs).toHaveBeenCalledTimes(1)
+    })
+  })
+})
