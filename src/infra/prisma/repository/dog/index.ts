a05154eeb6861d60c3830d2add16eb7e561e4d92
Eric Silva <ericsilvaccp@gmail.com>
2022-01-25 15:12:23 -0300

    fix(debit): remove debit

--- src/infra/prisma/repository/dog/index.ts
+++ src/infra/prisma/repository/dog/index.ts
@@ -2,12 +2,11 @@ import { DogModelResult, LoadDogsRepository } from '@data/protocols/dogs/show'
 import { prismaClient } from '@infra/prisma/client'
 
 export class DogRepositoryPrisma implements LoadDogsRepository {
-  constructor() {}
-
   async onLoadDogs(): Promise<DogModelResult[]> {
     const client = prismaClient
 
     const dogList = await client.dog.findMany()
+
     return dogList
   }
 }
